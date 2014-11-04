4a63215d1cc63fc58a57bf9e95e68e74e1f9ee69
Guilherme Tramontina <gtramontina@gtramontina-mbp.local>
2014-11-04 00:20:36 -0200

    support for nested objects

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 var through = require('through2')
 
 module.exports = function(opts) {
-  
+
   var trimmer = through.obj(function(ch, enc, next) {
-    var trimmed = {}
-    var keys = Object.keys(ch)
-    for (var i = 0; i < keys.length; i++) {
-      var k = keys[i]
-      var v = ch[k]
-      var trimk = k.trim()
-      if (typeof v === 'string') trimmed[trimk] = v.trim()
-      else trimmed[trimk] = v
-    }
-    trimmer.push(trimmed)
+    trimmer.push(trim(ch))
     next()
   })
-  
+
   return trimmer
+}
+
+function trim(obj) {
+  var trimmed = {}
+  var keys = Object.keys(obj)
+  for (var i = 0; i < keys.length; i++) {
+    var k = keys[i]
+    var v = obj[k]
+    var trimk = k.trim()
+    if (typeof v === 'string') trimmed[trimk] = v.trim()
+    else if (typeof v === 'object') trimmed[trimk] = trim(v)
+    else trimmed[trimk] = v
+  }
+  return trimmed
 }
